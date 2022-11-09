a842d3de9550af163c717e90183942b5e210dec6
Tungchaphet <93425627+Tungchaphet@users.noreply.github.com>
2022-11-09 21:24:05 +0700

    Delete dist directory

--- dist/lib/requests.d.ts
+++
@@ -1,8 +0,0 @@
-declare function requestHandlers(options: {
-    path: string;
-    method: 'GET' | 'POST' | 'PATCH' | 'PUT' | 'DELETE';
-    body?: object;
-    querys?: object;
-    auth?: string;
-}): Promise<any>;
-export { requestHandlers };
